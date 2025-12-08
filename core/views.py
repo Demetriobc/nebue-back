5d7994ebd880d8c275513fbaf29aeb72fc0e3a3c
Demetrio <demetriobernardocosta@hotmail.com>
2025-12-07 23:20:55 -0300

    tailwind

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@
 Core views for the Nebue application.
 """
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render  
 from django.views.generic import TemplateView
 
 
